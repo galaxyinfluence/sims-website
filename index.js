61b5e86689e71d6188222d3acbfc702a83a5424b
GalaxyInfluence <139978578+NotKFC-Real@users.noreply.github.com>
2025-03-17 10:12:04 +0000

    Update index.js

--- index.js
+++ index.js
@@ -24,3 +24,35 @@ db.getConnection((err) => {
     console.log("Connected to MySQL database.");
   }
 });
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(csurf({ cookie: true }));
+
+app.use((req, res, next) => {
+  const sessionToken = req.cookies.session_token;
+  if (sessionToken) {
+    db.query(
+      "SELECT * FROM users WHERE session_cookie =?",
+      [sessionToken],
+      (err, results) => {
+        if (!err && results.length > 0) {
+          req.user = {
+            Roblox_Id: results[0].Roblox_Id,
+            Roblox_Username: results[0].Roblox_Username,
+            Discord_Id: results[0].Discord_Id,
+            Discord_Username: reslts[0].Discord_Username,
+            Discord_ProfilePicture: results[0].Discord_ProfilePicture.
+          };
+        }
+        next();
+      }
+    );
+  } else {
+    next();
+  }
+});
